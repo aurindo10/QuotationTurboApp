3af063e17a2bfc4f28586a5dfe59d353d2d31a80
Aurindo Neto <aurindo15@gmail.com>
2023-05-03 15:05:36 -0300

    feat (add) drawer

--- apps/nextjs/src/pages/cotacoes.tsx
+++ apps/nextjs/src/pages/cotacoes.tsx
@@ -1,11 +1,16 @@
 import { ReactElement, useContext, useEffect } from "react";
+import { Drawer } from "../components/template/Drawer";
 import { NextPageWithLayout } from "./_app";
-import Drawer from "components/molecules/Drawer";
 
 const Page: NextPageWithLayout = () => {
   return <div className="text-[18px] text-white"> Olá</div>;
 };
 Page.getLayout = function IndexPage(page: ReactElement) {
-  return <Drawer>{page}</Drawer>;
+  return (
+    <div>
+      <Drawer></Drawer>
+      {page}
+    </div>
+  );
 };
 export default Page;
