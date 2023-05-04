9c0d00b5824f578e7b137dc9689b79d7c0c1f4b1
Aurindo Neto <aurindo15@gmail.com>
2023-05-04 00:02:16 -0300

    clean up

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,12 +1,8 @@
 import { router } from "../trpc";
-import { postRouter } from "./post";
 import { empresaRouter } from "./empresa";
-import { authRouter } from "./auth";
 
 export const appRouter = router({
-  post: postRouter,
   empresa: empresaRouter,
-  auth: authRouter,
 });
 
 // export type definition of API
