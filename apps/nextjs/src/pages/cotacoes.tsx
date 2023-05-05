a3d8db5f5ba93da1e0358db220ab26983060732a
Aurindo Neto <aurindo15@gmail.com>
2023-05-05 01:38:26 -0300

    feat (add) protection to pages (not working 100%)

--- apps/nextjs/src/pages/cotacoes.tsx
+++ apps/nextjs/src/pages/cotacoes.tsx
@@ -1,9 +1,19 @@
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 import { ReactElement, useContext, useEffect } from "react";
 import { Drawer } from "../components/template/Drawer";
 import { NextPageWithLayout } from "./_app";
 
 const Page: NextPageWithLayout = () => {
-  return <div></div>;
+  const router = useRouter();
+  const { user, isLoaded } = useUser();
+  if (!isLoaded) {
+    return <div className="text-slate-50">loading</div>;
+  }
+  if (!user!.publicMetadata.worker) {
+    router.push("/criarempresa");
+  }
+  return <div>Olá</div>;
 };
 Page.getLayout = function IndexPage(page: ReactElement) {
   return (
