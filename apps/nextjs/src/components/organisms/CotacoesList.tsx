5918988683f069b8503a857422cf051aae211bf6
Aurindo Neto <aurindo15@gmail.com>
2023-05-09 08:45:07 -0300

    feat (add) get all cotacoes endpoint

--- apps/nextjs/src/components/organisms/CotacoesList.tsx
+++ apps/nextjs/src/components/organisms/CotacoesList.tsx
@@ -1,32 +1,36 @@
+import { useUser } from "@clerk/nextjs";
 import { DotsThreeOutlineVertical } from "@phosphor-icons/react";
+import { trpc } from "../../utils/trpc";
 import DropdownMenuDemo from "../molecules/DropDown";
 export const CotacoesList = () => {
+  const { user } = useUser();
+  const { data, status } = trpc.cotacoes.getCotacoes.useQuery({
+    empresaId: user?.publicMetadata.idEmpresa as string,
+  });
+  if (status === "loading") {
+    return <div>loading</div>;
+  }
   return (
     <div className="flex w-full flex-col items-center ">
-      <div className="card card-compact bg-base-100 w-full max-w-xl shadow-xl">
-        <div className="card-body grid grid-cols-3">
-          <div className="col-span-2">
-            <h2 className="card-title">Cotação 01</h2>
-            <h3>Enviados: 01/03</h3>
-            <h3>Criado: 04/03/2023</h3>
+      {data?.map((cotacao) => {
+        return (
+          <div
+            className="card card-compact bg-base-100 w-full max-w-xl shadow-xl"
+            key={cotacao.id}
+          >
+            <div className="card-body grid grid-cols-3">
+              <div className="col-span-2">
+                <h2 className="card-title">{cotacao.nome}</h2>
+                <h3>{`Enviados: 01/03`}</h3>
+                <h3>Criado: 04/03/2023</h3>
+              </div>
+              <div className="card-actions justify-end">
+                <DropdownMenuDemo></DropdownMenuDemo>
+              </div>
+            </div>
           </div>
-          <div className="card-actions justify-end">
-            <DropdownMenuDemo></DropdownMenuDemo>
-          </div>
-        </div>
-      </div>
-      <div className="card card-compact bg-base-100 w-full max-w-xl shadow-xl">
-        <div className="card-body grid grid-cols-3">
-          <div className="col-span-2">
-            <h2 className="card-title">Cotação 01</h2>
-            <h3>Enviados: 01/03</h3>
-            <h3>Criado: 04/03/2023</h3>
-          </div>
-          <div className="card-actions justify-end">
-            <DropdownMenuDemo></DropdownMenuDemo>
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
