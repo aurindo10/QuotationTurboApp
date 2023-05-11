fbd23778a5e3d8984fcc82fee899f46bc2aae208
Aurindo Neto <aurindo15@gmail.com>
2023-05-10 23:25:39 -0300

    feat add search product to add created

--- apps/nextjs/src/pages/cotacoes/products/[id].tsx
+++ apps/nextjs/src/pages/cotacoes/products/[id].tsx
@@ -1,6 +1,8 @@
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/router";
 import { ReactElement, useContext, useEffect } from "react";
+import { useProductsOfCotationStore } from "../../../../zustandStore/ProductsOfCotationStore";
+import { InputToAddProductOnCotation } from "../../../components/molecules/AddProductInput";
 import { columns } from "../../../components/organisms/columns";
 import { DataTable } from "../../../components/template/data-table";
 import { Drawer } from "../../../components/template/Drawer";
@@ -11,23 +13,33 @@ const Page: NextPageWithLayout = () => {
   const router = useRouter();
 
   const { user, isLoaded } = useUser();
-  if (!isLoaded) {
-    return <div className="text-slate-50">loading</div>;
-  }
-  if (!user!.publicMetadata.nomeEmpresa) {
-    router.push("/criarempresa");
-  }
+  // if (!isLoaded) {
+  //   return <div className="text-slate-50">loading</div>;
+  // }
+  // if (!user!.publicMetadata.nomeEmpresa) {
+  //   router.push("/criarempresa");
+  // }
   const idEmpresa = user?.publicMetadata.idEmpresa as string;
+  const [addManyProducts, allProductsState] = useProductsOfCotationStore(
+    (state) => [state.addManyProducts, state.allProducts],
+  );
+
   const { data: allProducts, status: statusGettingProducts } =
     trpc.product.getProductsByCotation.useQuery({
       idEmpresa: idEmpresa,
       cotacaoId: (router.query.id as string) ?? "",
     });
+  useEffect(() => {
+    if (statusGettingProducts === "success" && allProducts) {
+      addManyProducts(allProducts);
+    }
+  }, [statusGettingProducts, allProducts]);
   if (statusGettingProducts === "loading") {
     return <div className=" text-slate-50"> Carregando...</div>;
   }
   return (
     <div className="w-full">
+      <InputToAddProductOnCotation></InputToAddProductOnCotation>
       <DataTable data={[allProducts!]} columns={columns}></DataTable>
     </div>
   );
