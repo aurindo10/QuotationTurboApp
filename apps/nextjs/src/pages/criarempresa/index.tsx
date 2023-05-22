48922d171151a3b7adac5be5751dc008da438ee8
Aurindo Neto <aurindo15@gmail.com>
2023-05-22 14:37:06 -0300

    feat (add) empresa cretaed protection on criarempresa page.

--- apps/nextjs/src/pages/criarempresa/index.tsx
+++ apps/nextjs/src/pages/criarempresa/index.tsx
@@ -1,4 +1,5 @@
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/router";
 import { ReactElement, useContext, useEffect } from "react";
 import { Alert } from "../../components/molecules/Alert";
 import { CreateEmpresaForm } from "../../components/organisms/CreateEmpresaForm";
@@ -7,6 +8,19 @@ import { Drawer } from "../../components/template/Drawer";
 import { NextPageWithLayout } from "../_app";
 
 const Page = () => {
+  const router = useRouter();
+  const { user, isLoaded } = useUser();
+
+  useEffect(() => {
+    if (user?.publicMetadata.idEmpresa && isLoaded) {
+      router.replace({
+        pathname: "/cotacoes",
+      });
+    }
+  }, [isLoaded, user, router]);
+  if (isLoaded === false || user?.publicMetadata.idEmpresa) {
+    return <div className="text-[30px]"> Carregando...</div>;
+  }
   return (
     <div className="h-screen w-full px-2 md:px-4 ">
       <div className="flex w-full justify-end px-2 py-2">
