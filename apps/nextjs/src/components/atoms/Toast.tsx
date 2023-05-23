2238b75223150cfb4997ceb4439f75f9e9d83e42
Aurindo Neto <aurindo15@gmail.com>
2023-05-23 13:48:02 -0300

    sadasdas

--- apps/nextjs/src/components/atoms/Toast.tsx
+++ apps/nextjs/src/components/atoms/Toast.tsx
@@ -12,7 +12,7 @@ export const ToastInfo = () => {
     <Toast.Provider swipeDirection="right">
       <Toast.Root
         className="data-[state=open]:animate-slideIn data-[state=closed]:animate-hide data-[swipe=end]:animate-swipeOut grid grid-cols-[auto_max-content] items-center gap-x-[15px] rounded-md bg-white p-[15px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] [grid-template-areas:_'title_action'_'description_action'] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=cancel]:translate-x-0 data-[swipe=cancel]:transition-[transform_200ms_ease-out]"
-        open={open}
+        open={open as boolean}
         onOpenChange={setOpen}
       >
         <Toast.Title className="text-slate12 mb-[5px] text-[15px] font-medium [grid-area:_title]">
